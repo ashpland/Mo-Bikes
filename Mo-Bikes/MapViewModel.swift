03aff541ce64c3d3e6a3339d1722678e7d204557
Andrew Shopland <hi@ashp.land>
2018-02-17 16:33:53 -0800

    Set proper colours

--- Mo-Bikes/MapViewModel.swift
+++ Mo-Bikes/MapViewModel.swift
@@ -87,7 +87,8 @@ extension Array where Element == Station {
 class StationMarker: MKMarkerAnnotationView {
     
     static let reuseID = "StationAnnotationView"
-    static let color = (normal: UIColor.blue, low: UIColor.cyan)
+    static let color = (normal: #colorLiteral(red: 0.5791940689, green: 0.1280144453, blue: 0.5726861358, alpha: 1),
+                        low: #colorLiteral(red: 0.7806914647, green: 0.5464840253, blue: 0.7773131953, alpha: 1))
     static let glyph = (bikes: "bikeIcon", docks: "dockIcon")
     
     let currentNumber = BehaviorSubject<String>(value: "0")
