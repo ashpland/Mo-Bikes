795f8da0b7978009c346fe2c42b8e74b2135db44
Andrew Shopland <hi@ashp.land>
2018-02-24 12:02:11 -0800

    Added temporary test for testStationValues. Needs to be replaced by proper UI test later

--- Mo-BikesTests/MapViewModelTests.swift
+++ Mo-BikesTests/MapViewModelTests.swift
@@ -22,6 +22,8 @@ class MapViewModelTests: XCTestCase {
     override func setUp() {
         super.setUp()
         
+        continueAfterFailure = false
+        
         stationManager = StationManager()
         
         let storyboard = UIStoryboard(name: "MapView",
@@ -30,12 +32,9 @@ class MapViewModelTests: XCTestCase {
         
         UIApplication.shared.keyWindow?.rootViewController = mapViewController
         
-        // Test and Load the View at the Same Time!
         XCTAssertNotNil(mapViewController.view)
         
-        mapView = mapViewController.mapView
-        
-        // set location and zoom
+        mapView = mapViewController.mapView        
         
         mapViewModel = MapViewModel(for: mapViewController, with: stationManager)
         
@@ -58,13 +57,13 @@ class MapViewModelTests: XCTestCase {
         
         let testStation = generateStation("Test Station", in: mapView.region)
         
+        let testAnnotation = testStation.annotation(in: mapView, with: mapViewModel.bikesOrSlots)
+        
         mapViewModel.display([testStation], in: mapView)
         
-        if let firstAnnotation = mapView.annotations.first {
-            let testStationCoordinate = CLLocationCoordinate2D(latitude: testStation.coordinate.lat, longitude: testStation.coordinate.lon)
-            XCTAssertEqual(firstAnnotation.coordinate, testStationCoordinate, "Annotation should have same coordinates as Station")
+        if let firstAnnotation = mapView.annotations.first as? StationAnnotation {
+            XCTAssertEqual(firstAnnotation, testAnnotation, "Adding annotation to mapView with display(_ in:) should produce same annotation")
         }
-        
     }
     
     func testDisplayMarker() {
@@ -72,14 +71,13 @@ class MapViewModelTests: XCTestCase {
 
         let testStation = generateStation("Test Station", in: mapView.region)
         
-        print(testStation.coordinate)
-        print(mapView.region)
-        
         mapViewModel.display([testStation], in: mapView)
         
         DispatchQueue.main.asyncAfter(deadline: .now() + 1, execute: {
-            let annotationContainerView = self.mapView.subviews[0].subviews[2]
-            XCTAssertTrue(annotationContainerView.subviews.count > 0)
+            let stationMarkerCount = self.mapView.subviews[0].subviews[2].subviews
+                .filter{$0 is StationMarker}
+                .count
+            XCTAssertTrue(stationMarkerCount == 1)
             expectMarker.fulfill()
         })
         
@@ -89,6 +87,61 @@ class MapViewModelTests: XCTestCase {
             }
         }
     }
+    
+    
+    
+    
+    // Replace this test with UITest
+    func testStationValues() {
+        
+        let expectValue = expectation(description: "Marker should display number of bikes")
+        
+        let testStation = generateStation("Test Station", in: mapView.region)
+        
+        mapViewModel.display([testStation], in: mapView)
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1, execute: {
+            let annotationContainerView = self.mapView.subviews[0].subviews[2]
+            let markerArray = annotationContainerView.subviews.filter({ (view) -> Bool in
+                view is StationMarker
+            })
+            
+            if let firstMarker = markerArray.first as? StationMarker {
+                firstMarker.setSelected(true, animated: true)
+                firstMarker.currentNumber
+                    .takeUntil(firstMarker.unsubscribeNumber)
+                    .subscribe(onNext: { firstMarker.glyphText = $0 })
+                    .disposed(by: self.disposeBag)
+
+                DispatchQueue.main.asyncAfter(deadline: .now() + 1, execute: {
+                    if let firstNumber = firstMarker.glyphText {
+                        
+                        do {
+                            let testNumber = try testStation.availableBikes.value()
+                            XCTAssertEqual(firstNumber, String(testNumber))
+                            expectValue.fulfill()
+                        }
+                        catch {
+                            XCTAssert(false, "Unable to retrieve available bikes value from testStation")
+                        }
+                    }
+                })
+            }
+        })
+        
+        waitForExpectations(timeout: 4) { error in
+            if let error = error {
+                XCTFail("waitForExpectationsWithTimeout errored: \(error)")
+            }
+        }
+        
+        
+        
+        
+        
+    }
+    
+    
 }
 
 
